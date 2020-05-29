c07a26198f8a8503441c55d526e867f38e2fc181
keniz01 <kenneth.kiiza>
2020-05-29 12:44:47 +0100

    Rectoring: Remove unused using references

--- src/infrastructure/Music.Library.Web/ClientApp/src/index.js
+++ src/infrastructure/Music.Library.Web/ClientApp/src/index.js
@@ -1,19 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import { BrowserRouter } from 'react-router-dom';
-//import { Layout } from './components/Layout';
-//import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Layout from './components/search/Layout'
 
-//const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  //<BrowserRouter basename={baseUrl}>
-    <Layout />,
-  //</BrowserRouter>,
+  <BrowserRouter basename={baseUrl}>
+    <App />,
+  </BrowserRouter>,
   rootElement);
 
 registerServiceWorker();
